ef0065eac53758b06d6224f666b8b5c5800d1686
Enzo <enzo.lpez.pstor@gmail.com>
2024-12-18 22:48:45 -0300

    style-fix

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -13,8 +13,8 @@ function App() {
     <>
       <div className='flex items-center justify-between bg-red-700 p-4'>
         <div className="flex items-center space-x-4">
-          <img className="p-2 sm:w-1/2 md:w-1/4 lg:w-1/6" src="../src/assets/pokemon-logo.png" alt="Pokemon Logo" />
-          <h1 className="text-3xl text-white">Pokedex</h1>
+          <img className="p-1 sm:w-1/2 md:w-1/4 lg:w-1/6" src="../src/assets/pokemon-logo.png" alt="Pokemon Logo" />
+          <h1 className="text-4xl text-white font-bold">Pokedex</h1>
         </div>
 
         <div className="flex items-center space-x-4">
--- app/src/components/PokemonItem.jsx
+++ app/src/components/PokemonItem.jsx
@@ -13,7 +13,7 @@ function PokemonItem({name, url, onClick}){
 
 	const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
 	return (
-		<div key={name} onClick={() => onClick(id)} className='cursor-pointer'>
+		<div key={name} onClick={() => onClick(id)} className='cursor-pointer p-2'>
 				<img className = 'w-24 sm:w-32 md:w-40 lg:w-48 xl:w-64 bg-slate-100 rounded-2xl mx-2 text-black' src = {sprite} alt = "Foto de Pokemon"></img>
 				<p className='text-black text-center font-bold'> {capitalizedName}</p>
 		</div>
